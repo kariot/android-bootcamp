c73e4b28102e618ff9fbed39fb27ec108fc6cdcd
Sreehari K <sreeharikariot@gmail.com>
2020-06-26 19:50:59 +0530

    Finished dashboard

--- LoginSample/app/src/main/java/com/kariot/loginsample/DashboardActivity.java
+++ LoginSample/app/src/main/java/com/kariot/loginsample/DashboardActivity.java
@@ -8,12 +8,14 @@ import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
     TextView myData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         myData = findViewById(R.id.textView);
-        myData.setText(getIntent().getStringExtra("UNAME"));
-        Log.e("USERNAME IS",getIntent().getStringExtra("UNAME"));
+        myData.setText("Username is :" + getIntent().getStringExtra("UNAME") + "\n" +
+                "Password is : " + getIntent().getStringExtra("PWORD"));
+        Log.e("USERNAME IS", getIntent().getStringExtra("UNAME"));
     }
 }
